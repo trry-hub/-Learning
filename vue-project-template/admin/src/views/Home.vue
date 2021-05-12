8596ee97f742be10975d5cc9d1195d7ef3793b13
shizhu <trrymail@163.com>
2021-05-12 17:26:57 +0800

    添加视频播放器

--- vue-project-template/admin/src/views/Home.vue
+++ vue-project-template/admin/src/views/Home.vue
@@ -2,35 +2,69 @@
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png">
         <HelloWorld msg="Welcome to Your Vue.js App" />
+
+        <dir id="dplayer" />
     </div>
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import DPlayer from 'dplayer'
 export default {
     name: 'Home',
     components: {
         HelloWorld
     },
+    data() {
+        return {
+        }
+    },
     mounted() {
         this.getData()
+        this.initDPlayer()
     },
     methods: {
+        initDPlayer() {
+            new DPlayer({
+                container: document.getElementById('dplayer'),
+                screenshot: true,
+                video: {
+                    url: 'https://video.yaomaitong.net/356f2c7840bd4305a6367c7dcf4530d0/e38ce0183dfd4055b6aed463bb1aa5f3-9f6a5769eb52428c674845bf84d214e2-ld.mp4'
+                }
+            })
+        },
+
+        // player is ready
+        playerReadied(player) {
+            console.log('the player is readied', player)
+            // you can use it to do something...
+            // player.[methods]
+        },
         async getData() {
             try {
                 // const res = await this.$get('/login', { params: { name: 'zhangsan' } })
                 // console.log(res)
-            
+
                 console.log(this.$store)
-            
+
                 const postres = await this.$post('/login', { name: 'zhangsan' })
                 console.log(postres)
             } catch (error) {
                 console.log(error)
-                
+
             }
         }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+*{
+    margin: 0;
+    padding: 0
+}
+#dplayer {
+    width: 900px;
+    margin: 0 auto;
+}
+</style>

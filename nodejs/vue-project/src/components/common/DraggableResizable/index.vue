8fcbb63746d59e26c6c203331e64b23c6a1d8d2f
shizhu <trrymail@163.com>
2021-07-27 13:53:17 +0800

    添加自有拖拽组件

---
+++ nodejs/vue-project/src/components/common/DraggableResizable/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
+        <vue-draggable-resizable v-bind="$attrs" :w="100" :h="100" :parent="true" v-on="$listeners" @dragging="onDrag" @resizing="onResize">
+            <p>
+                Hello! I'm a flexible component. You can drag me around and you can resize me.<br>
+                X: {{ x }} / Y: {{ y }} - Width: {{ width }} / Height: {{ height }}
+            </p>
+        </vue-draggable-resizable>
+    </div>
+</template>
+ 
+<script>
+import VueDraggableResizable from 'vue-draggable-resizable'
+
+export default {
+    name: 'DraggableResizable',
+    components: {
+        VueDraggableResizable
+    },
+    data() {
+        return {
+            width: 0,
+            height: 0,
+            x: 0,
+            y: 0
+        }
+    },
+    methods: {
+        onResize(x, y, width, height) {
+            this.x = x
+            this.y = y
+            this.width = width
+            this.height = height
+        },
+        onDrag(x, y) {
+            this.x = x
+            this.y = y
+        }
+    }
+}
+</script>
--- nodejs/vue-project/src/components/common/PageContent/index.vue
+++ nodejs/vue-project/src/components/common/PageContent/index.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="page-content">
         <keep-alive>
-            <draggable v-model="list" group="people" :animation="0" :disabled="false" fallback-on-body @start="drag=true" @end="drag=false">
-                <component :is="item.componentName" v-for="(item,index) in list" :key="item.id+index" class="draggable-item component-wrapper" :class="id === item.id?'active':''" v-bind="item.bindDataTotag" v-on="item.bindToMethodss">
-                    {{ item.name }}
-                </component>
-            </draggable>
+            <DraggableResizable :active="true" :resizable="true" />
+            <!-- <draggable v-model="list" group="people" :animation="0" active :disabled="false" fallback-on-body @start="drag = true" @end="drag = false">
+                    <component :is="item.componentName" class="draggable-item component-wrapper" :class="id === item.id ? 'active' : ''" v-bind="item.bindDataTotag" v-on="item.bindToMethodss">
+                        {{ item.name }}
+                    </component>
+                </draggable> -->
         </keep-alive>
     </div>
 </template>
@@ -36,7 +37,11 @@ export default {
                     methods: {},
                     computed: {}
                 }
-            ]
+            ],
+            width: 0,
+            height: 0,
+            x: 0,
+            y: 0
         }
     },
     watch: {
@@ -50,6 +55,16 @@ export default {
     methods: {
         getList() {
             return this.list
+        },
+        onResize(x, y, width, height) {
+            this.x = x
+            this.y = y
+            this.width = width
+            this.height = height
+        },
+        onDrag(x, y) {
+            this.x = x
+            this.y = y
         }
     }
 }
@@ -58,20 +73,20 @@ export default {
 <style scoped lang="scss">
 // scss
 .page-content {
-	height: 100%;
-	padding: 10px;
-	box-sizing: border-box;
-	.component-wrapper {
-		display: inline-block;
-		border: 1px solid transparent;
-		&active {
-			border: 1px solid $theme-color;
-		}
-	}
+    height: 100%;
+    padding: 10px;
+    box-sizing: border-box;
+    .component-wrapper {
+        display: inline-block;
+        border: 1px solid transparent;
+        &active {
+            border: 1px solid $theme-color;
+        }
+    }
 
-	::v-deep .draggable-item:active {
-		border: 1px solid $theme-color;
-		cursor: move;
-	}
+    ::v-deep .draggable-item:active {
+        border: 1px solid $theme-color;
+        cursor: move;
+    }
 }
 </style>

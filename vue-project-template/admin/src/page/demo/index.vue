92e772fef4fc2fadaf425060fd963b3d1c45f23d
trry <trrymail@163.com>
2021-08-15 03:06:22 +0800

    vue-admin 测试表格

--- vue-project-template/admin/src/page/demo/index.vue
+++ vue-project-template/admin/src/page/demo/index.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
         这是测试的：{{ }}
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column type="index" :index="indexMethod">
+            </el-table-column>
+            <el-table-column prop="date" label="日期" width="180">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="180">
+            </el-table-column>
+            <el-table-column prop="address" label="地址">
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 <script>
@@ -8,9 +18,52 @@ export default {
     name: 'Demo',
     props: {},
     data() {
-        return {}
+        return {
+            tableData: [
+                {
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333,
+                    tag: '家'
+                },
+                {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1517 弄',
+                    zip: 200333,
+                    tag: '公司'
+                },
+                {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1519 弄',
+                    zip: 200333,
+                    tag: '家'
+                },
+                {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1516 弄',
+                    zip: 200333,
+                    tag: '公司'
+                }
+            ]
+        }
     },
     mounted() {},
-    methods: {}
+    methods: {
+        indexMethod(index) {
+            return index * 2
+        }
+    }
 }
 </script>

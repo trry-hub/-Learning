53e28b23a6ced5fc779ab9ba2054a63f3742f85f
trry <trrymail@163.com>
2021-09-19 23:57:53 +0800

    优化vue3模板

--- vite-react/src/assets/svg/index.js
+++
@@ -1,8 +0,0 @@
-// const requireAll = (requireContext) => requireContext.keys().map(requireContext);
-// const svgs = require.context("./",false,/\.svg$/);
-// requireAll(svgs);
-
-const requireAll = import.meta.globEager("./*svg");
-for (const path in requireAll) {
-    requireAll[path]
-}
--- vite-react/src/layouts/BlogList/components/Posts/index.jsx
+++
@@ -1,9 +0,0 @@
-import postsstyle from './index.module.scss'
-
-import React,{ Component } from 'react'
-
-export default class Posts extends Component {
-    render() {
-        return <div className={ postsstyle["posts-wrap"] }>Posts组件</div>
-    }
-}

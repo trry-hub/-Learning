502b90054dc7c45f8cb75c65af9b35e2e745f7ad
trry <trrymail@163.com>
2021-10-21 18:12:33 +0800

    更新docs

--- vuepress-starter/docs/.vuepress/config.js
+++ vuepress-starter/docs/.vuepress/config.js
@@ -5,7 +5,6 @@ function genSidebarConfig(title) {
             title,
             collapsable: false,//是否可折叠
             children: [ //文章目录，构建出来的网页会按照顺序链接
-                '',
                 'getting-started',
                 'basic-config',
                 'assets',
@@ -23,6 +22,35 @@ module.exports = {
     title: 'trry-docs',
     description: 'this is a docs',
     base: "/docs/",
+    head: [
+        ['link',{rel:'icon',href: '/logo.png'}]
+    ],
+    themeConfig: {
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: 'https://github.com/wubaoliang',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: '查看源码',
+    
+        // 以下为可选的编辑链接选项
+    
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        docsRepo: 'vuejs/vuepress',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'master',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
+        editLinkText: '帮助我们改善此页面！',
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Guide', link: '/'},
+            {text: 'Home', link: '/'},
+            {text: 'Home', link: '/'},
+        ]
+      }
     // nav: [
     //     {
     //         text: 'Guide',

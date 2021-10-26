c5ea4ce1703b1e081983cd6827436fb24ad07ce1
Bliangjs <Bliangjs@163.com>
2021-10-26 10:01:48 +0800

    修改自动打包

--- vitepress/docs/.vitepress/config.js
+++ vitepress/docs/.vitepress/config.js
@@ -19,50 +19,36 @@ function genSidebarConfig(title) {
 }
 
 module.exports = {
-    title: 'trry-docs',
+    title: 'Bliangjs',
     description: 'this is a docs',
-    base: '/docs',
-    // themeConfig: {
-    //     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    //     repo: 'https://github.com/Bliangjs',
-    //     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    //     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    //     repoLabel: '查看源码',
-    
-    //     // 以下为可选的编辑链接选项
-    
-    //     // 假如你的文档仓库和项目本身不在一个仓库：
-    //     docsRepo: 'vuejs/vitepress',
-    //     // 假如文档不是放在仓库的根目录下：
-    //     docsDir: 'docs',
-    //     // 假如文档放在一个特定的分支下：
-    //     docsBranch: 'master',
-    //     // 默认是 false, 设置为 true 来启用
-    //     editLinks: true,
-    //     // 默认为 "Edit this page"
-    //     editLinkText: '帮助我们改善此页面！',
-    //     nav: [
-    //         {text: 'Home', link: '/'},
-    //         {text: 'Guide', link: '/'},
-    //         {text: 'Home', link: '/'},
-    //         {text: 'Home', link: '/'},
-    //     ]
-    //   }
-    // nav: [
-    //     {
-    //         text: 'Guide',
-    //         link: '/guide/',
-    //     },
-    //     {
-    //         text: 'Config Reference',
-    //         link: projects
-    //     },
-    //     {
-    //         text: 'Default Theme Config',
-    //         link: projects
-    //     }
-    // ],
-    // sidebar: {
-    //     '/guide/': genSidebarConfig('指南')//设定侧边栏的函数，参数是名称
-    // },
+    base: '/docs/',
+    themeConfig: {
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: 'https://github.com/Bliangjs',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: 'GitHub',
+
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        // docsRepo: 'vuejs/vitepress',
+        // 假如文档不是放在仓库的根目录下：
+        // docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        // docsBranch: 'master',
+        // 默认是 false, 设置为 true 来启用
+        // editLinks: true,
+        // 默认为 "Edit this page"
+        // editLinkText: '帮助我们改善此页面！',
+        nav: [
+            { text: 'Home', link: '/home' },
+            { text: 'Guide', link: '/guide' },
+            { text: 'Home', link: '/' },
+            { text: 'Home', link: '/' },
+        ],
+    },
+    sidebar: {
+        '/guide/': genSidebarConfig('指南') //设定侧边栏的函数，参数是名称
+    },
 }
--- vitepress/docs/zh/guide/index.md
+++ vitepress/docs/zh/guide/index.md
@@ -1,12 +1,11 @@
 ---
 title: tags
-date: 2019-08-13 09:39:50
+date: 2021-10-26 09:39:50
 type: tags
 layout: tag
 ---
 
 
 # Hello VitePress
-
 
 # {{ $frontmatter.date }}

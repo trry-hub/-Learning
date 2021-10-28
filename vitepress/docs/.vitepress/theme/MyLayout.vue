7fc81fe41ebe49b7d610e6e6c635ff650c2211d1
Bliangjs <Bliangjs@163.com>
2021-10-28 10:26:28 +0800

    添加产品图

---
+++ vitepress/docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <Layout>
+        <!-- <template #sidebar-top>
+            My custom sidebar top content
+        </template> -->
+    </Layout>
+</template>
+
+<!--.vitepress/theme/MyLayout.vue-->
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+const { Layout } = DefaultTheme
+</script>

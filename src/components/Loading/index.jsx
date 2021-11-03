1f7f451e14533528bec1129fca5b1db7b2f92919
Bliangjs <Bliangjs@163.com>
2021-11-03 14:37:36 +0800

    修改loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,27 +1,20 @@
-import css from "./index.module.scss";
+import css from './index.module.scss'
 
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export default class Loading extends Component {
 	render() {
 		return (
-			<div className={css["loading-wrap"]}>
-				<div className={css["loading"]}>
-					<span style={{ "--i": 1 }}></span>
-					<span style={{ "--i": 2 }}></span>
-					<span style={{ "--i": 3 }}></span>
-					<span style={{ "--i": 4 }}></span>
-					<span style={{ "--i": 5 }}></span>
-					<span style={{ "--i": 6 }}></span>
-					<span style={{ "--i": 7 }}></span>
+			<div className={css['loading-wrap']}>
+				<div className={css['loader']}>
+					<div className={css['circle']}></div>
+					<div className={css['circle']}></div>
+					<div className={css['circle']}></div>
+					<div className={css['circle']}></div>
+					<div className={css['circle']}></div>
+					<div className={css['circle']}></div>
 				</div>
-				<svg>
-					<filter id="gooey">
-						<feGaussianBlur in="SourceGraphic" stdDeviation="10" />
-						<feColorMatrix values=" 1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 20 -5 " />
-					</filter>
-				</svg>
 			</div>
-		);
+		)
 	}
 }
